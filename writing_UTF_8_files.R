3c042b2051e4e112a7783303028cf7426b4a9444
pverkind <peter.verkinderen@gmail.com>
2022-04-28 18:03:37 +0200

    update write file

--- writing_UTF_8_files.R
+++ writing_UTF_8_files.R
@@ -1,8 +1,14 @@
+# writing results of your code to a file is not simple on Windows
+# because of issues with Arabic. 
+
 v <- c("ABC", "ابج")
 
-# writing the results to a file is not simple on Windows
-# because of issues with Arabic. This is a workaround
-# (writing a string as bytes):
+# one approach: use the write_lines or write_file functions from the package readr: 
+library(readr)
+write_lines(v, "test.txt")  # first argument: character vector, second argument: file path
+write_file(paste(v, collapse="\n"), "test.txt")
+
+# This is a workaround (writing a string as bytes):
 result <- paste(v, collapse="\n\n") # collapse the vector into a single string
 writeBin(charToRaw(result), choose.files(), endian="little")
 
